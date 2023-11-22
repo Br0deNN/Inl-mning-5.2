ec7370c5f59a836caed8a9a05e57acc9c189afbe
7R1XY <theo.broden@gmail.com>
2023-11-22 10:08:22 +0100

    Add project files.

---
+++ Inlämning 5.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace inlämming_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int input = int.Parse(Console.ReadLine());
+            int[] x = {1, 2, input};
+
+            Console.WriteLine(x[0] + " " + x[1] + " " + x[2]);
+            
+            
+
+            
+        }
+    }
+}
